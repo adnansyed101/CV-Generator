cb60b7751e4c49d53966c3768c94f2c47cd70824
adnansyed101 <sirgeant10@gmail.com>
2023-03-10 12:16:32 +0600

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ const App = () => {
         <h1 className="text-center text-4xl text-white">CV Generator</h1>
       </header>
       <Body />
+      <footer className="bg-slate-800 py-4">
+        <h4 className="text-center text-white">
+          Copyright &#169; {new Date().getFullYear()}{" "}
+          <a href="https://github.com/adnansyed101" target='_blank'>Md Adnan</a>{" "}
+        </h4>
+      </footer>
     </div>
   );
 };
