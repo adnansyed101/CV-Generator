d376b5520a781575b5798a08b2371d28c199e33b
adnansyed101 <sirgeant10@gmail.com>
2023-03-10 15:12:53 +0600

    change the width of the form

--- src/components/CvForm.jsx
+++ src/components/CvForm.jsx
@@ -20,7 +20,7 @@ const CvForm = (props) => {
   return (
     <form
       onSubmit={handleSubmit}
-      className="container mx-auto my-10 md:p-10 md:w-4/5 w-11/12 border-2 p-5 shadow"
+      className="container mx-auto my-10 md:p-10 md:w-4/6 w-11/12 border-2 p-5 shadow"
     >
       <fieldset>
         <legend>Personal Info</legend>
